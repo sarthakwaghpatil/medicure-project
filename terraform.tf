81e8f715a2a0753b88488937642776d441297f12
root <root@ip-172-31-21-80.ec2.internal>
2023-03-19 13:21:16 +0000

    added

--- terraform.tf
+++ terraform.tf
@@ -1,23 +1,130 @@
-node{
-    stage('checkout the code'){
-        git 'https://github.com/sarthakwaghpatil/finance-project.git'
+#Initialize Terraform
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
     }
-    stage('terraform init'){
-        sh 'sudo terraform init'
-    }
-    stage('terraform validate'){
-        sh 'terraform validate'
-    }
-    stage('terraform plan'){
-       withCredentials([string(credentialsId: 'AWS_ACCESS_KEY_ID', variable: 'AWS_ACCESS_KEY_ID')]) {
-
-           withCredentials([string(credentialsId: 'AWS_SECRET_ACCESS_KEY', variable: 'AWS_SECRET_ACCESS_KEY')]) {
-    
-                sh 'terraform plan'
-            }
-        }
-    }
-    stage('terraform apply'){
-        sh 'terraform apply --auto-approve'
-    }
+  }
 }
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "proj-vpc" {
+  cidr_block = "10.0.0.0/16"
+} 
+# Create an Internet Gateway
+resource "aws_internet_gateway" "proj-ig" {
+vpc_id = aws_vpc.proj-vpc.id
+tags = {
+Name = "gateway1"
+}
+}
+# Setting up the route table
+resource "aws_route_table" "proj-rt" {
+vpc_id = aws_vpc.proj-vpc.id
+route {
+# pointing to the internet
+cidr_block = "0.0.0.0/0"
+gateway_id = aws_internet_gateway.proj-ig.id
+}
+route {
+ipv6_cidr_block = "::/0"
+gateway_id = aws_internet_gateway.proj-ig.id
+}
+tags = {
+Name = "rt1"
+}
+}
+# Setting up the subnet
+resource "aws_subnet" "proj-subnet" {
+vpc_id = aws_vpc.proj-vpc.id
+cidr_block = "10.0.1.0/24"
+availability_zone = "us-east-1b"
+tags = {
+Name = "subnet1"
+}
+}
+# Associating the subnet with the route table
+resource "aws_route_table_association" "proj-rt-sub-assoc" {
+subnet_id = aws_subnet.proj-subnet.id
+route_table_id = aws_route_table.proj-rt.id
+}
+# Creating a Security Group
+resource "aws_security_group" "proj-sg" {
+name = "proj-sg"
+description = "Enable web traffic for the project"
+vpc_id = aws_vpc.proj-vpc.id
+ingress {
+description = "HTTPS traffic"
+from_port = 443
+to_port = 443
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+ingress {
+description = "HTTP traffic"
+from_port = 0
+to_port = 65000
+protocol = "tcp"
+cidr_blocks = ["0.0.0.0/0"]
+}
+egress {
+from_port = 0
+to_port = 0
+protocol = "-1"
+cidr_blocks = ["0.0.0.0/0"]
+ipv6_cidr_blocks = ["::/0"]
+}
+tags = {
+Name = "proj-sg1"
+}
+}
+# Creating a new network interface
+resource "aws_network_interface" "proj-ni" {
+subnet_id = aws_subnet.proj-subnet.id
+private_ips = ["10.0.1.10"]
+security_groups = [aws_security_group.proj-sg.id]
+}
+# Attaching an elastic IP to the network interface
+resource "aws_eip" "proj-eip" {
+vpc = true
+network_interface = aws_network_interface.proj-ni.id
+associate_with_private_ip = "10.0.1.10"
+}
+# Creating an ubuntu EC2 instance
+resource "aws_instance" "proj-instance" {
+ami = "ami-0557a15b87f6559cf"
+instance_type = "t2.micro"
+availability_zone = "us-east-1b"
+key_name = "mykeypair"
+network_interface {
+device_index = 0
+network_interface_id = aws_network_interface.proj-ni.id
+}
+
+
+
+
+
+
+ user_data  = <<-EOF
+ #!/bin/bash
+     sudo apt-get update -y
+     sudo apt install docker.io -y
+     sudo systemctl enable docker
+     sudo docker run -itd -p 8081:8081 sarthakwaghpatil/medicure:1.0 
+     sudo docker run -p 9090:9090 prom/prometheus
+     sudo docker run -d -p 3000:3000 grafana/grafana-enterprise
+     sudo systemctl daemon-reload
+     sudo systemctl start grafana-server
+ EOF
+ tags = {
+ Name = "Prod-Server"
+ }
+}
+
